baa433591eb193f0f9f1ac26fa1b64c1e8b28e84
iqtidar234 <iqtidar2345@gmail.com>
2023-08-27 14:20:25 +0500

    routing bug

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -8,19 +8,19 @@ import Contact from '../pages/Contact';
 import Blog from '../pages/Blog';
 
 const Routing = () => {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path='/' element={<Layout/>}>
-                  <Route path='/' element={<Home />}/>
-                  <Route path='/about' element={<About />}/>
-                  <Route path='/blog' element={<Blog/>}></Route>
-                  <Route path='/services' element={<Services />}></Route>
-                  <Route path='/contact' element={<Contact />}></Route>
-                  </Route>
-          </Routes>
-      </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/blog' element={<Blog />} />
+                    <Route path='/services' element={<Services />} />
+                    <Route path='/contact' element={<Contact />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default Routing
